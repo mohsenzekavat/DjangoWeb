cabaafd9f0b8617856d4e29e12f6fdc96edd1848
mohsenzekavat <mohsenzekavat84@gmail.com>
2026-09-14 22:13:29 +0330

    Rename project configuration from WebDjango to DjangoWeb

--- DjangoWeb/asgi.py
+++ DjangoWeb/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for WebDjango project.
+ASGI config for DjangoWeb project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoWeb.settings')
 
 application = get_asgi_application()
--- DjangoWeb/wsgi.py
+++ DjangoWeb/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for WebDjango project.
+WSGI config for DjangoWeb project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoWeb.settings')
 
 application = get_wsgi_application()
